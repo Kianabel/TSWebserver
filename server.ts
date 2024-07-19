e13269aeb2780322bd27cbc26077735f4e7403cc
Kianabel <kian.leba@gmail.com>
2024-07-19 09:01:06 +0200

    added reload capability

--- server.ts
+++ server.ts
@@ -21,18 +21,16 @@ const PORT: number = 22222;
 const IP: string = "127.0.0.1";
 const BACKLOG: number = 521;
 
-const htmlbody = fs.readFileSync("./public/index.html").toString() //converting entry html into string
-
-const responseHeaders = new Map<string, string>(); //html headers
-responseHeaders.set("Content-Type", "text/html");
-responseHeaders.set("Content-Length", htmlbody.length.toString());
-
-const response: Response = {
-  protocol: "HTTP/1.1",
-  headers: responseHeaders,
-  status: "OK",
-  statusCode: 200,
-  body: htmlbody,
+const getBody = (): Promise<string> => {    //get new file content
+  return new Promise((resolve, reject) => {
+    fs.readFile("./public/index.html", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data.toString());
+      }
+    });
+  });
 };
 
 const compileResponse = (r: Response): string => {
@@ -44,10 +42,31 @@ const compileResponse = (r: Response): string => {
 
 net
   .createServer((socket) => {
-    socket.on("data", (buffer) => {
+    socket.on("data", async (buffer) => {
       const request = buffer.toString();
-      socket.write(compileResponse(response));
-      socket.end();
+
+      try {
+        const bodyHTML = await getBody();
+
+        const responseHeaders = new Map<string, string>(); //html headers
+        responseHeaders.set("Content-Type", "text/html");
+        responseHeaders.set("Content-Length", bodyHTML.length.toString());
+
+        const response: Response = {
+          protocol: "HTTP/1.1",
+          headers: responseHeaders,
+          status: "OK",
+          statusCode: 200,
+          body: bodyHTML,
+        };
+
+        socket.write(compileResponse(response));
+        socket.end();
+      } catch (err) {
+        console.error("Error reading file:", err);
+        socket.write("HTTP/1.1 500 Internal Server Error\r\n\r\n");
+        socket.end();
+      }
     });
   })
   .listen(PORT, IP, BACKLOG, () => {
